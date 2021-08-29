9713e9e365a0519d1fd737ce861318cdf32f4985
Azariagmt <azariagebremichael10@gmail.com>
2021-08-29 11:20:35 +0300

    DVC:sets repo url to github

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -22,7 +22,7 @@ logger = log(path="../logs/", file="causal-graph.logs")
 logger.info("Starts Causal graph script")
 
 train_store_path = 'data/data.csv'
-repo = "../"
+repo = "https://github.com/Azariagmt/Causality/"
 
 version = "v1"
 
